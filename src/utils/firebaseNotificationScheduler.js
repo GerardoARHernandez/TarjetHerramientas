c444086832f5b85d23bc31638c414075d53c4157
Gerardo Antonio Ruelas Hernández <gahdz14@gmail.com>
2026-01-21 17:31:41 -0600

    Prueba de firebase 4

--- src/utils/firebaseNotificationScheduler.js
+++ src/utils/firebaseNotificationScheduler.js
@@ -3,7 +3,7 @@ import { firebaseConfig, messaging, checkFirebaseSupport } from '../firebase/con
 import { getToken, onMessage, deleteToken } from 'firebase/messaging';
 
 export class FirebaseNotificationScheduler {
-  constructor(hour = 18, minute = 0) {
+  constructor(hour = 17, minute = 33) {
     this.hour = hour;
     this.minute = minute;
     this.timeoutId = null;
